4e8a1791c7ce9ca0e0374660ad0b3f5556e0f86e
triciahughes <hughes.a.tricia@gmail.com>
2023-01-20 11:09:59 -0800

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+//////// Variables Needed //////
+const form = document.querySelector("#create-task-form");
+const div = document.querySelector("#list");
+const tasksList = document.querySelector("#tasks");
+
+////// Form Submit Event ///////
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listItem = document.createElement("li");
+    tasksList.appendChild(listItem);
+    const description = e.target["new-task-description"].value;
+    listItem.textContent = description;
+    form.reset();
+  });
 });
